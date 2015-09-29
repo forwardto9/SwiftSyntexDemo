d78a9d892431fde96383007792478d3db393e1cd
forwardto9 <forwarto9@hotmail.com>
2015-09-29 11:33:59 +0800

    Initial Commit

---
+++ SwiftSyntexDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSyntexDemo
+//
+//  Created by uwei on 9/29/15.
+//  Copyright © 2015 Tencent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

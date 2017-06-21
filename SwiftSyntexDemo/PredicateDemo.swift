ff07cee9afa254a3c923f2fd46eeaffc5f469637
uwei <forwardto9@gmail.com>
2017-06-21 11:25:26 +0800

    add Predicate subclass

--- SwiftSyntexDemo/PredicateDemo.swift
+++ SwiftSyntexDemo/PredicateDemo.swift
@@ -8,16 +8,19 @@
 
 import Foundation
 
+fileprivate let firstNamekey = "firstName"
+fileprivate let lastNamekey = "lastName"
+fileprivate let agekey = "age"
 
-class PersonDemo: NSObject {
+class PersonDemo: NSObject,NSCoding {
     var firstName: NSString
     var lastName: NSString
-    var age: NSNumber
+    var age: Int
     
     init(firstName: String, lastName: String, age: Int) {
         self.firstName = firstName as NSString
         self.lastName = lastName as NSString
-        self.age = NSNumber(value: age)
+        self.age = Int(NSNumber(value: age))
     }
     
     override var description: String {
@@ -27,6 +30,18 @@ class PersonDemo: NSObject {
     override func value(forUndefinedKey key: String) -> Any? {
         print(key)
         return "age"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.firstName = aDecoder.decodeObject(forKey: firstNamekey) as! NSString
+        self.lastName  = aDecoder.decodeObject(forKey: lastNamekey) as! NSString
+        self.age       = Int(aDecoder.decodeObject(forKey: agekey) as! NSNumber)
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.firstName, forKey: firstNamekey)
+        aCoder.encode(self.lastName, forKey: lastNamekey)
+        aCoder.encode(NSNumber(value: self.age), forKey: agekey)
     }
 }
 
@@ -47,6 +62,14 @@ class PredicateDemo:NSObject {
         let numPredicate1 = NSPredicate(format: "SELF < 10", argumentArray:nil);
         let r1 = (numbersArray as NSArray).filtered(using: numPredicate1)
         print(r1)
+        
+//        let lE1 = NSExpression(forConstantValue: numbersArray)
+//        let rE1 = NSExpression(forConstantValue: 10)
+//        let cP1 = NSComparisonPredicate(leftExpression: lE1, rightExpression: rE1, modifier: .direct, type: .lessThan, options: .normalized)
+//        print((numbersArray as NSArray).filtered(using: cP1))
+        
+        
+        
         let numPredicate2 = NSPredicate(format: "SELF BETWEEN {0, 10}", argumentArray:nil);
         print((numbersArray as NSArray).filtered(using: numPredicate2))
         
@@ -64,9 +87,60 @@ class PredicateDemo:NSObject {
         print((namesArray as NSArray).filtered(using: p5))
         
         //MARK:- Set
-        print(alice.value(forKeyPath: "age") ?? "xxx")
-        let p6 = NSPredicate(format: "all age>20", argumentArray: nil)
-        print((people as NSArray).filtered(using: p6))
+        let p6 = NSPredicate(format: "age in {30,31, 32,33}", argumentArray: nil)
+        print((people).filtered(using: p6))
+        
+        
+        //MARK:- Object set
+        let p7 = NSPredicate(format: "age >= 31", argumentArray: nil)
+        print((people).filtered(using: p7))
+        
+        let lE7 = NSExpression(forKeyPath: "age")
+        let rE7 = NSExpression(forConstantValue: 31)
+        let cp7 = NSComparisonPredicate(leftExpression: lE7, rightExpression: rE7, modifier: .direct, type: .greaterThanOrEqualTo, options: .normalized)
+        print((people).filtered(using: cp7))
+        
+        
+        
+        
+        
+        let p8 = NSPredicate(format: "age >= 31 && age <= 33", argumentArray: nil)
+        print((people).filtered(using: p8))
+        
+        let lE71 = NSExpression(forKeyPath: "age")
+        let rE71 = NSExpression(forConstantValue: 33)
+        let cp71 = NSComparisonPredicate(leftExpression: lE71, rightExpression: rE71, modifier: .direct, type: .lessThanOrEqualTo, options: .normalized)
+        let cp8 = NSCompoundPredicate(andPredicateWithSubpredicates: [cp7, cp71])
+        print((people).filtered(using: cp8))
+        
+        
+        let p9 = NSPredicate(format: "firstName like[cd] 'Bo*'", argumentArray: nil)
+        print((people).filtered(using: p9))
+        
+        let property = "lastName"
+        let propertyValue = "smith"
+        let p10 = NSPredicate(format: "%K contains[cd] %@", argumentArray: [property, propertyValue])
+        print((people).filtered(using: p10))
+        
+        let ageProperty = "age"
+        let agePropertyValue = 30
+        let tempP11 = NSPredicate(format: "%K < $value", argumentArray: [ageProperty])
+        let p11 = tempP11.withSubstitutionVariables(["value":agePropertyValue])
+        print((people).filtered(using: p11))
+        
+        let p12 = tempP11.withSubstitutionVariables(["value":33])
+        print((people).filtered(using: p12))
+        
+        
+        
+        //MARK:- Expression function
+        let sumExpression = NSExpression(forFunction: "stddev:", arguments: [NSExpression(forConstantValue: numbersArray)])
+        let result = sumExpression.expressionValue(with: nil, context: nil)
+        print(result ?? "result is nil")
+        
+        
+        exit(1)
+        
     }
     
 }
